b74003427405adc7603d9669687754a98eed5065
Abhishek Balodi <abhishekbalodi1189@gmail.com>
2025-09-18 13:57:40 +0530

    fix product-list

--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -30,12 +30,12 @@
 <!-- Product List -->
 <div class="card">
     <div class="card-header d-flex align-items-center justify-content-between flex-wrap row-gap-3">
-        <!-- <div class="search-set"> -->
+        <div class="search-set">
             <div class="search-input">
-                <input type="text" class="form-control" placeholder="Search..." id="search-input">
+                <!-- <input type="text" class="form-control" placeholder="Search..." id="search-input"> -->
                 <span class="btn-searchset"><i class="ti ti-search fs-14 feather-search"></i></span>
             </div>
-        <!-- </div> -->
+        </div>
         <div class="d-flex table-dropdown my-xl-auto right-content align-items-center flex-wrap row-gap-3">
             <div class="dropdown me-2">
                 <a href="javascript:void(0);" class="dropdown-toggle btn btn-white btn-md d-inline-flex align-items-center" data-bs-toggle="dropdown">
