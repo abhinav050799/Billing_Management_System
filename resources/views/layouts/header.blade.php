241437e8896606f2e397cbe6a5b9a2945cfdad5c
Abhishek Balodi <abhishekbalodi1189@gmail.com>
2025-09-22 17:36:08 +0530

    changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -552,7 +552,7 @@
                                 <li><a href="expired-products.html"><span>Expired Products</span></a></li>
                                 <li><a href="low-stocks.html"><span>Low Stocks</span></a></li>
                                 <li><a href="{{ route('categories.index') }}"><span>Category</span></a></li>
-                                <li><a href="sub-categories.html"><span>Sub Category</span></a></li>
+                                <li><a href="{{ route('subcategories.index') }}"><span>Sub Category</span></a></li>
                                 <li><a href="brand-list.html"><span>Brands</span></a></li>
                                 <li><a href="units.html"><span>Units</span></a></li>
                                 <li><a href="varriant-attributes.html"><span>Variant Attributes</span></a></li>
--- resources/views/sub-categories.blade.php
+++ resources/views/sub-categories.blade.php
@@ -41,7 +41,7 @@
                 <span class="btn-searchset"><i class="ti ti-search fs-14 feather-search"></i></span>
             </div>
         </div>
-        <div class="d-flex table-dropdown my-xl-auto right-content align-items-center flex-wrap row-gap-3">
+        <!-- <div class="d-flex table-dropdown my-xl-auto right-content align-items-center flex-wrap row-gap-3">
             <div class="dropdown me-2">
                 <a href="javascript:void(0);" class="dropdown-toggle btn btn-white btn-md d-inline-flex align-items-center" data-bs-toggle="dropdown">
                     Category
@@ -54,7 +54,7 @@
                     @endforeach
                 </ul>
             </div>
-        </div>
+        </div> -->
     </div>
     <div class="card-body p-0">
         <div class="table-responsive">
