acfbeacfdd64b8e7e302715a0d9243e0a3b5a796
Abhishek Balodi <abhishekbalodi1189@gmail.com>
2025-09-25 17:21:39 +0530

    register in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,8 +38,8 @@
         </div>
 
         <div class="mt-4 text-center">
-    <a href="{{ route('employee.login') }}" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        {{ __('Login as Employee') }}
+    <a href="{{ route('register') }}" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        {{ __('Register') }}
     </a>
 </div>
 
